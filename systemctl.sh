ae73ab16f398dc3cc947bc5c9569248763c0e344
Rose1440 <68105821+dky4@users.noreply.github.com>
2025-08-30 11:45:46 +0200

    Update systemctl.sh

--- systemctl.sh
+++ systemctl.sh
@@ -6,18 +6,25 @@ echo "Installing SystemCTL"
 if ! command -v python3 >/dev/null 2>&1; then
     echo "Python3 isn't installed!"
     if [ -f /etc/debian_version ]; then
-        apt-get update
-        apt-get install -y python3 curl
+        DEBIAN_FRONTEND=noninteractive apt-get update
+        DEBIAN_FRONTEND=noninteractive apt-get install -y python3 curl
     fi
 fi
 
-# Download systemctl.py directly from the repository
 if [ ! -f /usr/bin/systemctl ]; then
-    curl -sSL https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl.py -o /usr/bin/systemctl
+    curl -sSL https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl3.py -o /usr/bin/systemctl
     chmod +x /usr/bin/systemctl
 else
     echo "systemctl is already installed!!!"
 fi
 
+if [ ! -f /usr/bin/journalctl ]; then
+    curl -sSL https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/journalctl3.py -o /usr/bin/journalctl
+    chmod +x /usr/bin/journalctl
+else
+    echo "journalctl is already installed!!!"
+fi
+
 clear
 echo "Done! You can now use systemctl"
+
